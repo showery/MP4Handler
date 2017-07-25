a83ea4ff0fa417369847896e4427112d3f412fbf
John.Huang <John@johndeMac.local>
2017-07-25 18:20:29 +0800

    delete objs

--- Test/app/src/main/obj/local/armeabi-v7a/objs-debug/mp4v2/interface/common.o.d
+++
@@ -1,8 +0,0 @@
-/Users/john/Documents/android/Test/app/mSrc/main/obj/local/armeabi-v7a/objs-debug/mp4v2/interface/common.o: \
-  /Users/john/Documents/android/Test/app/mSrc/main/jni/interface/common.cpp \
-  /Users/john/Documents/android/Test/app/mSrc/main/jni/interface/common.h \
-  /Users/john/Documents/android/Test/app/mSrc/main/jni/common/typedef.h
-
-/Users/john/Documents/android/Test/app/mSrc/main/jni/interface/common.h:
-
-/Users/john/Documents/android/Test/app/mSrc/main/jni/common/typedef.h:
