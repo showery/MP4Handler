499bafd96e8c7370d9383ed3f85c00b94095cf98
John.Huang <John@johndeMac.local>
2017-08-01 16:32:33 +0800

    delete some file

--- Test/app/src/main/obj/local/armeabi-v7a/objs-debug/interface/source/jnicommon.o.d
+++
@@ -1,7 +0,0 @@
-./obj/local/armeabi-v7a/objs-debug/interface/source/jnicommon.o: \
-  jni/interface/source/jnicommon.cpp jni/interface/include/jnicommon.h \
-  jni/interface/../common/include/typedef.h
-
-jni/interface/include/jnicommon.h:
-
-jni/interface/../common/include/typedef.h:
